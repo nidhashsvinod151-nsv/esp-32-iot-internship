fcc8a0110718e1675fc3ebd0b4071442d9006080
nidhashsvinod151-nsv <nidhashsvinod151@gmail.com>
2026-06-10 12:03:05 +0530

    Create day06_ota_update.ino

---
+++ task3/week1-day06/day06_ota_update.ino
@@ -0,0 +1,32 @@
+#include <WiFi.h>
+#include <ArduinoOTA.h>
+#include "secrets.h"
+
+#define OTA_HOSTNAME  "esp32-zelbytes-lab"
+#define OTA_PASSWORD  "zelbytes2026"
+
+int otaProgress = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("================================");
+  Serial.println("  Day 06 - OTA Update Manager");
+  Serial.println("  Zelbytes IoT Lab");
+  Serial.println("================================");
+
+  connectWiFi();
+  setupOTA();
+
+  Serial.println("Ready. Waiting for OTA...");
+}
+
+void connectWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to WiFi");
+  unsigned long startTime = millis();
+
+  wh
