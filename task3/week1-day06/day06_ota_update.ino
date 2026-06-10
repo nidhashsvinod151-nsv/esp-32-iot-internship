360c00be0891efb67b2078292a32b8a96b62ceb6
nidhashsvinod151-nsv <nidhashsvinod151@gmail.com>
2026-06-10 17:16:43 +0530

    Update day06_ota_update.ino

--- task3/week1-day06/day06_ota_update.ino
+++ task3/week1-day06/day06_ota_update.ino
@@ -2,31 +2,35 @@
 #include <ArduinoOTA.h>
 #include "secrets.h"
 
-#define OTA_HOSTNAME  "esp32-zelbytes-lab"
-#define OTA_PASSWORD  "zelbytes2026"
-
-int otaProgress = 0;
-
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-
-  Serial.println("================================");
-  Serial.println("  Day 06 - OTA Update Manager");
-  Serial.println("  Zelbytes IoT Lab");
-  Serial.println("================================");
-
-  connectWiFi();
-  setupOTA();
-
-  Serial.println("running..");
-}
-
-void connectWiFi() {
+  delay(3000);
+  Serial.println("--- STARTING ---");
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
   Serial.print("Connecting to WiFi");
   unsigned long startTime = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (millis() - startTime > 15000) {
+      Serial.println("\nWiFi FAILED!");
+      Serial.print("Status code: ");
+      Serial.println(WiFi.status());
+      return;
+    }
+  }
+  Serial.println("\nWiFi connected!");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+  ArduinoOTA.setHostname("esp32-zelbytes-lab");
+  ArduinoOTA.setPassword("zelbytes2026");
+  ArduinoOTA.begin();
+  Serial.println("OTA ready!");
+}
 
-  wh
+void loop() {
+  ArduinoOTA.handle();
+  Serial.println("Loop running - OTA v2!");
+  delay(5000);
+}
